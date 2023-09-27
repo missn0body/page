ee519f8471c4d02324da310f77daee6051fb82e6
missn0body <hulk128t@gmail.com>
2023-09-27 10:18:13 -0700

    Added terminal configuration and manipulation

--- 1.2.0/page.cpp
+++ 1.2.0/page.cpp
@@ -63,6 +63,9 @@ namespace
 // I would rather you call the macro below
 void _error(const char *file, int line, const char *what)
 {
+	write(STDOUT_FILENO, "\033[2J", 4);
+        write(STDOUT_FILENO, "\033[H", 3);
+
 	fprintf(std::cerr, "(%d:%d) %s: %s\n"_p, file, line, what, strerror(errno));
 	exit(EXIT_FAILURE);
 }
@@ -76,9 +79,15 @@ void _error(const char *file, int line, const char *what)
 class pg
 {
 	private:
+		int rows, cols;
+		int curX, curY;
+		int rowOff, colOff;
+		std::string filename;
+
 		struct termios original, raw;
 		std::vector<std::string> scrBuf;
 
+		bool rawMode = false;
 	public:
 		pg();
 		~pg();
@@ -87,23 +96,92 @@ class pg
 		void revert();
 };
 
-pg::pg() { setRaw(); }
-pg::~pg(){ revert(); }
+// Cause this is what member initialization looks like :)
+pg::pg() : rows(0), cols(0), curX(0), curY(0), rowOff(0), colOff(0),
+	   filename("(null)")
+{
+	setRaw();
+}
 
-void pg::setRaw() { printf("setRaw()"); return; };
-void pg::revert() { printf("revert"); return; };
+pg::~pg() { revert(); }
+
+void pg::revert()
+{
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &this->original) == -1) error("tcsetattr");
+};
+
+// As antirez says: "1960s magic shit" below.
+void pg::setRaw()
+{
+	if(this->rawMode == true) return;
+	if(tcgetattr(STDIN_FILENO, &this->original) == -1) error("tcgetattr");
+	this->raw = this->original;
+
+	// Disable most of all signal processing, parity checking,
+	// output processing, terminal echo, canonical mode,
+	// and most of what the CTRL key does.
+	this->raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	this->raw.c_oflag &= ~(OPOST);
+	// We also ensure that each character being sent is 8 bits.
+	this->raw.c_cflag |=  (CS8);
+	this->raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	// So that read() doesn't halt the program to wait for input.
+	this->raw.c_cc[VMIN] = 0;
+	this->raw.c_cc[VTIME] = 1;
+
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &this->raw) == -1) error("tcsetattr");
+	this->rawMode = true;
+};
+
+void refresh()
+{
+	write(STDOUT_FILENO, "\033[2J", 4);
+	write(STDOUT_FILENO, "\033[H", 3);
+}
+
+char readch()
+{
+	int result;
+	char returnType;
+
+	while((result = read(STDIN_FILENO, &returnType, 1)) == -1)
+	{
+		if(result == -1 && errno != EAGAIN) error("read");
+	}
+	return returnType;
+}
+
+// Returns a request for an exit, true for exit request and
+// false for otherwise. The reason we're doing this is so
+// that the deconstructor for 'pg' has the chance to actually
+// run, rather than exiting here
+bool parseKeys()
+{
+	char in = readch();
+	switch(in)
+	{
+		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, "\033[2J", 4);
+        		write(STDOUT_FILENO, "\033[H", 3);
+			return false;
+	}
+
+	return true;
+}
 
 //////////////////////////////////////
 // main()
 //////////////////////////////////////
 
-int main(int argc, char *argv[])
+int main()
 {
-	if(argc < 2)
+	pg mainScreen;
+
+	while(1 > 0)
 	{
-		fprintf(std::cerr, "%s: too few arguments\n"_p, argv[0]);
-		exit(EXIT_FAILURE);
+		refresh();
+		if(parseKeys() == false) break;
 	}
 
-	exit(EXIT_SUCCESS);
+	return 0;
 }
